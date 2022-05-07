c37a89887efc4f916ebd464b42250baf81420340
Carlos M <carlosedm@protonmail.com>
2022-05-07 14:42:58 -0600

    WIP form

--- pages/newPost.js
+++ pages/newPost.js
@@ -4,7 +4,37 @@ import Editor from 'rich-markdown-editor'
 const newPost = () => {
   return (
     <>
-      <Editor defaultValue="Hello" id="editor" onFocus={console.log(this.value)} />
+      <form action="" method="post">
+        <div>
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            Título
+          </h1>
+          <input type="text" name="" id="" className="mx-auto" />
+        </div>
+        <hr className="my-5" />
+        <div>
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            Resumen
+          </h1>
+          <input type="text" name="" id="" />
+        </div>
+        <hr className="my-5" />
+        <div>
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            Cuerpo
+          </h1>
+          <Editor id="editor" dark="true" onChange={(func) => console.log('edit: ' + func())} />
+        </div>
+        <hr className="my-5" />
+        <div>
+          <h1 className="text-2xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-3xl sm:leading-10 md:text-4xl md:leading-14">
+            Tags
+          </h1>
+        </div>
+        <button type="submit" className="mx-5">
+          Postear
+        </button>
+      </form>
     </>
   )
 }
